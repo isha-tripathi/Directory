df57883552da803ddce4c008492635eb095834fd
isha-tripathi <ishatrip@thoughtworks.com>
2014-09-02 14:10:05 +0530

    Adding entries as resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resource 'entries'
+  resources :entries
 
   root to: 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
